aebd649267636465ce7d9cca8415be9b6efb90ee
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-07-23 18:28:26 +0900

    [Bronze II] Title: 오각형, 오각형, 오각형…, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Bronze/1964. 오각형， 오각형， 오각형…/오각형， 오각형， 오각형….java
+++ 백준/Bronze/1964. 오각형， 오각형， 오각형…/오각형， 오각형， 오각형….java
@@ -1,19 +1,22 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+				
 		long num = Integer.parseInt(br.readLine());
-		long result = 1;
 		
-		for(int i = 1; i <= num; i++) {
-			result = result + 3*i + 1 ;
-		}
+		sb.append(((1 + num + 3*(num*(num+1)) /2) % 45678));
 		
-		System.out.println(result % 45678);
+		bw.write(String.valueOf(((1 + num + 3*(num*(num+1)) /2) % 45678)));
+		bw.flush();
+		
 	}
 }
