14a337a63329293c75d38d1ef24aad51184f16b3
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-07-26 22:09:40 +0900

    [Bronze II] Title: Hashing, Time: 76 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -7,16 +7,17 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		int length = Integer.parseInt(br.readLine());
-		
+
 		String hash = br.readLine();
-		
-		int result = 0;
-		
-		for(int i = 0; i < length; i++) {
-			result += ((hash.charAt(i) - 96) * Math.pow(31, i));
+
+		long result = 0;
+		long pow = 1;
+		for (int i = 0; i < length; i++) {
+			result += ((hash.charAt(i) - 96) * pow % 1234567891);
+			pow = pow * 31 % 1234567891;
 		}
-		
-		System.out.println(result % 1234567891);
-		
+
+		System.out.println(result);
+
 	}
 }
