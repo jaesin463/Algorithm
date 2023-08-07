97d8314699c22d58a6f226cd05af912bf99ed3dd
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-08-07 17:43:50 +0900

    [Bronze I] Title: 딱지놀이, Time: 284 ms, Memory: 28524 KB -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.java
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.java
@@ -6,42 +6,34 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		StringBuilder sb = new StringBuilder();
 		int round = Integer.parseInt(br.readLine());
-		char[] result = new char[round];
-
-		for (int i = 0; i < round; i++) {
-
-			int[][] child = new int[2][5];
-
-			for (int j = 0; j < 2; j++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				st.nextToken();
-				while (st.countTokens() != 0) {
-					child[j][Integer.parseInt(st.nextToken())]++;
+		
+		for(int i = 0;i<round;i++) {
+			String winner = "D";
+			int[] Acard = new int[4]; // 네모0 세모1 동그라미2 별3
+			int[] Bcard = new int[4];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int Anum = Integer.parseInt(st.nextToken());
+			for(int n = 0;n<Anum;n++) {
+				Acard[Integer.parseInt(st.nextToken())-1]++;
+			}
+			st = new StringTokenizer(br.readLine());
+			int Bnum = Integer.parseInt(st.nextToken());
+			for(int n = 0;n<Bnum;n++) {
+				Bcard[Integer.parseInt(st.nextToken())-1]++;
+			}
+			for(int j = 3;j>=0;j--) {
+				if(Acard[j]>Bcard[j]) {
+					winner = "A";
+					break;
+				} else if(Acard[j]<Bcard[j]) {
+					winner = "B";
+					break;
 				}
 			}
-			
-			if(child[0][4] != child[1][4]) {
-				if(child[0][4] > child[1][4]) {
-					result[i] = 'A';
-				} else result[i] = 'B';
-			} else if(child[0][3] != child[1][3]) {
-				if(child[0][3] > child[1][3]) {
-					result[i] = 'A';
-				} else result[i] = 'B';
-			} else if(child[0][2] != child[1][2]) {
-				if(child[0][2] > child[1][2]) {
-					result[i] = 'A';
-				} else result[i] = 'B';
-			} else if(child[0][1] != child[1][1]) {
-				if(child[0][1] > child[1][1]) {
-					result[i] = 'A';
-				} else result[i] = 'B';
-			} else result[i] = 'D';
-			
-			System.out.println(result[i]);
+			sb.append(winner).append("\n");
 		}
-
+		System.out.println(sb);
 	}
 }
