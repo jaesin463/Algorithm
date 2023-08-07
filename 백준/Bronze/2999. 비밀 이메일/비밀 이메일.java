ae2a167a9eef81de037f552f80681a4f12268c2f
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-08-07 17:12:07 +0900

    [Bronze I] Title: 비밀 이메일, Time: 84 ms, Memory: 11440 KB -BaekjoonHub

--- 백준/Bronze/2999. 비밀 이메일/비밀 이메일.java
+++ 백준/Bronze/2999. 비밀 이메일/비밀 이메일.java
@@ -1,32 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-		int N = str.length();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String message = br.readLine();
+
+		int N = message.length();
+
 		int R = 0;
 		int C = 0;
-		for(int i = 1;i<=Math.sqrt(N);i++) {
-			if(N%i==0) {
-				R = Math.max(R, i);
-				C = N/R;
-			}
-		}
-		char[][] arr = new char[C][R];
-		int idx = 0;
-		for(int i = 0;i<arr.length;i++) {
-			for(int j = 0;j<arr[0].length;j++) {
-				arr[i][j] = str.charAt(idx++);
-			}
-		}
-		String result = "";
-		for(int j = 0;j<arr[0].length;j++) {
-			for(int i = 0;i<arr.length;i++) {
-				result += arr[i][j];
-			}
-		}
-		System.out.println(result);
-	}
 
+		for (int i = 1; i <= Math.sqrt(N); i++) {
+			if (N % i == 0) {
+				R = i;
+				C = N / i;
+			}
+		}
+
+		char[][] secret = new char[R][C];
+
+		int idx = 0;
+
+		for (int i = 0; i < C; i++) {
+			for (int j = 0; j < R; j++) {
+				secret[j][i] = message.charAt(idx++);
+			}
+		}
+		
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				System.out.print(secret[i][j]);
+			}
+		}
+
+
+	}
 }
