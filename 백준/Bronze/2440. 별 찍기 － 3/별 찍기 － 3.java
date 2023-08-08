ac3687308f0f40035490f4bd040887ced2e2db70
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-08-08 17:17:16 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 172 ms, Memory: 12664 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = N; i > 0; i--) {
+			for(int j = i; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
