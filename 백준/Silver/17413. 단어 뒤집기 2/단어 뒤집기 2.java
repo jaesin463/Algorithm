8ad06be5287c7923ef2e8f7da3c9c25abd249bd7
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-08-09 15:16:37 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 208 ms, Memory: 16836 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,68 +1,40 @@
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Stack;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static Stack<String> s = new Stack<String>();
-
-	public static void rWord() {
-		while (!s.empty()) {
-			sb.append(s.pop());
-		}
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] wordArr = br.readLine().split("");
-		String word;
-		int where = 5;
-		int idx = 0;
-		// StringTokenizer의 남은 토큰이 0일 때까지
-		while (idx < wordArr.length) {
-			// 다음 토큰
-			word = wordArr[idx];
-
-			if (word.equals("<"))
-				where = 1;
-			else if (word.equals(">"))
-				where = 2;
-			else if (idx == wordArr.length - 1)
-				where = 4;
-			else if (word.equals(" "))
-				if (where != 1)	where = 3;
-
-			switch (where) {
-			case 1:
-				rWord();
-				sb.append(word);
-				break;
-			case 2:
-				sb.append(word);
-				where = 5;
-				break;
-			case 3:
-				rWord();
-				sb.append(word);
-				where = 5;
-				break;
-			case 4:
-				sb.append(word);
-				rWord();
-				break;
-			case 5:
-				s.push(word);
-				break;
+		String str = br.readLine() + " ";
+		StringBuilder result = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
+		boolean B = false;
+		for(int i = 0;i<str.length();i++) {
+			char ch = str.charAt(i);
+			if(ch=='<') {
+				result.append(sb.reverse());
+				sb.delete(0, sb.length());
+				sb.append(ch);
+				B = true;
+			} else if(ch=='>') {
+				sb.append(ch);
+				result.append(sb);
+				sb.delete(0, sb.length());
+				B = false;
+			} else if(ch==' ' && B) {
+				sb.append(ch);
+			} else if(ch==' ' && !B) {
+				result.append(sb.reverse()).append(" ");
+				sb.delete(0, sb.length());
+			} else {
+				sb.append(ch);
 			}
-			idx++;
 		}
-		bw.write(String.valueOf(sb));
-		bw.flush();
-		bw.close();
+		System.out.println(result);
 	}
+	
+	
+
 }
