aff61d6cc3cdb95656b349e7ffa103d6018f872d
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-08-18 09:34:37 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 162 ms, Memory: 29,460 KB -BaekjoonHub

---
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static int[][] map;
+    public static boolean[][] check;
+    public static int[] start;
+    public static int[] dr = { -1, 0, 1, 0 };
+    public static int[] dc = { 0, 1, 0, -1 };
+    public static int result;
+
+    public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st;
+	for (int tc = 1; tc <= 10; tc++) {
+	    br.readLine();
+	    
+	    map = new int[100][100];
+	    check = new boolean[100][100];
+	    result = 0;
+	    
+	    for (int i = 0; i < 100; i++) {
+		String str = br.readLine();
+		for (int j = 0; j < 100; j++) {
+		    map[i][j] = Integer.parseInt(String.valueOf(str.charAt(j)));
+		    if (map[i][j] == 2)
+			start = new int[]{i, j};
+		}
+	    }
+	    check[start[0]][start[1]] = true;
+	    dfs(start[0], start[1]);
+	    
+	    System.out.printf("#%d %d\n", tc, result);
+	}
+    }
+
+    static void dfs(int row, int col) {
+	for (int i = 0; i < dr.length; i++) {
+	    int nr = row + dr[i];
+	    int nc = col + dc[i];
+
+	    if (nr >= 100 || nr < 0 || nc >= 100 || nc < 0)
+		continue;
+
+	    if (check[nr][nc])
+		continue;
+
+	    if (map[nr][nc] == 1)
+		continue;
+
+	    if (map[nr][nc] == 3) {
+		result = 1;
+		return;
+	    }
+	    
+	    check[nr][nc] = true;
+	    dfs(nr, nc);
+	}
+    }
+    
+    static int init(String str) {
+	return Integer.parseInt(str);
+    }
+}
