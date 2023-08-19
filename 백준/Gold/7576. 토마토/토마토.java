85b5da1f8d9d450329d24b6600824321f5e1d76d
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-08-20 01:20:15 +0900

    [Gold V] Title: 토마토, Time: 680 ms, Memory: 120700 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,95 +1,77 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	static int[][] tomato;
+	static int row, col;
+	static int[][] map;
 	static boolean[][] visited;
-	static Queue<Point> queue = new LinkedList<>();
+	static int[] dr = { 1, 0, -1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
+	static Queue<int[]> q = new LinkedList<>();
+	static int max = 0;
+
+	static void BFS() {
+
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int dx = now[0] + dr[i];
+				int dy = now[1] + dc[i];
+
+				if (dx >= 0 && dx < row && dy >= 0 && dy < col) {
+					if (!visited[dx][dy] && map[dx][dy] == -2) {
+						q.add(new int[] { dx, dy });
+						visited[dx][dy] = true;
+						map[dx][dy] = map[now[0]][now[1]] + 1;
+						max = Math.max(max, map[dx][dy]);
+					}
+				}
+			}
+		}
+
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		col = init(st.nextToken());
+		row = init(st.nextToken());
+		map = new int[row][col];
+		visited = new boolean[row][col];
 
-		// 토마토가 담겨진 상자
-		tomato = new int[M][N];
-		// 토마토가 익었는지 여부
-		visited = new boolean[M][N];
-
-		// 토마토 위치 입력
-		for (int i = 0; i < tomato.length; i++) {
+		for (int i = 0; i < row; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < tomato[0].length; j++) {
-				tomato[i][j] = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < col; j++) {
+				map[i][j] = init(st.nextToken());
+				if (map[i][j] == 1) {
+					q.add(new int[] { i, j });
+					visited[i][j] = true;
+					map[i][j] = 0;
+				} else if(map[i][j] == 0) {
+					map[i][j] = -2;
+				}
 			}
 		}
 
-		// 토마토가 없는 칸이라면 방문 표시 && 토마토가 있는 칸이라면 방문 표시 후 큐에 삽입
-		for (int i = 0; i < tomato.length; i++) {
-			for (int j = 0; j < tomato[0].length; j++) {
-				if(tomato[i][j] == -1) {
-					visited[i][j] = true;
-				}
-				if(tomato[i][j] == 1) {
-					visited[i][j] = true;
-					queue.offer(new Point(i,j));
-					tomato[i][j] = 0;
+		BFS();
+		
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if(map[i][j] == -2) {
+					System.out.println(-1);
+					return;
 				}
 			}
 		}
-		// 탐색 시작
-		bfs();
-		
-		int max = 0;
-		
-		// 안익은 토마토가 있다면 최댓값을 -1 로 갱신 && 그렇지 않다면 최댓값 갱신
-		loop1 : for (int i = 0; i < tomato.length; i++) {
-			for (int j = 0; j < tomato[0].length; j++) {
-				if(!visited[i][j]) {
-					max = -1;
-					break loop1;
-				} else {
-					max = Math.max(tomato[i][j], max);
-				}
-			}
-			
-		}
-			
 		System.out.println(max);
-		
 	}
-	public static void bfs() {
-		// 큐가 빌때까지 반복
-		while(!queue.isEmpty()) {
-			// 큐에서 이전 토마토 위치 꺼내기
-			Point p = queue.poll();
-			// 4방으로 탐색
-			for(int i=0; i<4; i++) {
-				int nx = p.x + dx[i];
-				int ny = p.y + dy[i];
-				// 토마토 창고를 벗어나지 않으면서 익지않은 토마토가 있다면 익힌 후 큐에 삽입 , 날짜 입력
-				if(nx>=0 && ny>=0 && nx<tomato.length && ny<tomato[0].length && !visited[nx][ny]) {
-					visited[nx][ny] = true;
-					tomato[nx][ny] = tomato[p.x][p.y] + 1;
-					queue.offer(new Point(nx,ny));
-				}
-			}
-		}
+
+	static int init(String str) {
+		return Integer.parseInt(str);
 	}
 }
-
-// 1. 토마토가 심어진 창고 배열 생성 / 방문 여부 표시 배열까지 생성
-// 2. 만일 토마토가 들어있지 않은 칸이 있다면 방문표시
-// 3. 시작 지점에서 4방을 탐색하면서 카운팅
-// 4. 모든 지점이 방문표시 되어있다면 가장 배열에서 가장 큰 카운팅값 출력
-// 5. 방문하지 않은 지점이 존재한다면 -1 출력
