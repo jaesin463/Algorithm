be464322aefc20648afbd4a8cadb60f32a32bb61
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-08-23 00:27:24 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 104 ms, Memory: 12780 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			String str = sc.next();
+		}
+		
+		System.out.println("비와이");
+	}
+
+	static int init(String str) {
+		return Integer.parseInt(str);
+	}
+}
