0814511051d670070aa9017cc70b53fabc1a789a
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-08-23 00:42:55 +0900

    [Bronze V] Title: 홍익대학교, Time: 112 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int var = sc.nextInt();
+		var = var - 1946;
+		System.out.println(var);
+	}
+}
