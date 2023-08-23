de284f1ea2721f5740ec9a4f5a256a5aba668282
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-08-23 10:57:59 +0900

    [Bronze V] Title: Multiply, Time: 564 ms, Memory: 52332 KB -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		br.readLine();
+		
+		BigInteger n1 = new BigInteger(br.readLine());
+		BigInteger n2 = new BigInteger(br.readLine());
+		
+		System.out.println(n1.multiply(n2));
+	}
+
+	static int init(String str) {
+		return Integer.parseInt(str);
+	}
+}
