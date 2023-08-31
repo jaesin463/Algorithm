7a89366cd257b70fa18a54fccf9f4abf7f680ae6
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-09-01 01:42:59 +0900

    [Platinum V] Title: 히스토그램, Time: 296 ms, Memory: 23132 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static int N;
+	static int[] square;
+
+	static void solution() {
+		long max = 0;
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < N; i++) {
+			while (!stack.isEmpty() && square[stack.peek()] >= square[i]) {
+				long height = square[stack.pop()];
+				long width = 0;
+				if (stack.isEmpty())
+					width = i;
+				else
+					width = i - 1 - stack.peek();
+				max = Math.max(max, height * width);
+			}
+			stack.push(i);
+		}
+
+		while (!stack.isEmpty()) {
+			long height = square[stack.pop()];
+			long width = 0;
+			if (stack.isEmpty())
+				width = N;
+			else
+				width = N - 1 - stack.peek();
+			max = Math.max(max, height * width);
+		}
+		sb.append(max + "\n");
+	}
+
+	static void make() throws IOException {
+			N = init(br.readLine());
+			square = new int[N];
+			for (int i = 0; i < N; i++) {
+				square[i] = init(br.readLine());;
+			}
+			solution();
+	}
+
+	public static void main(String[] args) throws IOException {
+		make();
+		System.out.println(sb);
+	}
+
+	static int init(String str) {
+		return Integer.parseInt(str);
+	}
+}
