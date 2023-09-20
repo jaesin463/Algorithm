5a3b63b494a38192b314435b711c64086c269b7e
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-09-20 09:46:31 +0900

    [Gold III] Title: 프렉탈 평면, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Gold/1030. 프렉탈 평면/프렉탈 평면.java
+++ 백준/Gold/1030. 프렉탈 평면/프렉탈 평면.java
@@ -4,31 +4,24 @@ import java.io.*;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	// 변수
 	static int s, N, K, R1, R2, C1, C2;
 	static int start, end;
 	static boolean[][] result;
+	static int cnt = 0;
 
-	// 분할정복
 	static void partition(int r, int c, int len) {
-//		System.out.println("cnt : " + cnt + " r : " + r + " c : " + c + " len : " + len + " bw : " + bw);
-//		System.out.println("r + len : " + (r + len) + " c + len : " + (c + len));
-//		System.out.println("R1 : " + R1 + " C1 : " + C1 + " R2 : " + R2 + " C2 : " + C2);
-		// r시작점이 R2보다 작거나, r끝점이 R1보다 클 때
-		// c시작점이 C2보다 작거나, c끝점이 C1보다 클 때
+		// len이 N^s부터 시작하기 때문에 아래 조건에 걸리면 구하고자 하는 영역에 못 들어감 
 		if (r > R2 || c > C2 || r + len <= R1 || c + len <= C1) {
 			return;
 		}
-//		System.out.println("No Return");
-		// s만큼 재귀하면 num을 넣고 리턴
+		// 이때까지 검은색이 된 경우가 없기 때문에 흰색(true) 처리
 		if (len == 1) {
-			result[r - R1][c - C1] = false;
+			result[r - R1][c - C1] = true;
 			return;
 		}
 
 		int nextLen = len / N;
 
-		// 재귀
 		for (int i = 0; i < N; i++) {
 			for (int k = 0; k < N; k++) {
 				if (i >= start && i <= end && k >= start && k <= end)
@@ -41,15 +34,13 @@ public class Main {
 	}
 
 	static void solution() {
-		// 계산
 		int len = (int) Math.pow(N, s);
 		partition(0, 0, len);
 
-		// 출력
 		StringBuilder sb = new StringBuilder();
 		for (boolean[] ar : result) {
 			for (boolean a : ar) {
-				sb.append(a ? 1 : 0);
+				sb.append(a ? 0 : 1);
 			}
 			sb.append("\n");
 		}
@@ -59,29 +50,26 @@ public class Main {
 	static void make() throws IOException {
 		st = new StringTokenizer(br.readLine());
 
-		s = Integer.parseInt(st.nextToken()); // 시간 -> 0 ~ 10
-		N = Integer.parseInt(st.nextToken()); // 나누어지는 단위 -> 3 ~ 8
-		K = Integer.parseInt(st.nextToken()); // 검정색으로 채울 부분
+		s = init(st.nextToken());
+		N = init(st.nextToken());
+		K = init(st.nextToken());
 
-		// 전체 배열을 만들면?
-		// len(N^s)의 최대 크기 : 8^10 = 약 10억
-		// 배열의 최대 크기 = 10억*10억 = 100억
-		// 따라서 -> 정답 배열만 만들기
-		R1 = Integer.parseInt(st.nextToken());
-		R2 = Integer.parseInt(st.nextToken());
-		C1 = Integer.parseInt(st.nextToken());
-		C2 = Integer.parseInt(st.nextToken());
-		result = new boolean[R2 - R1 + 1][C2 - C1 + 1]; // 정답을 저장할 배열
-		for(int i = 0; i < R2 - R1 + 1; i++) {
-			Arrays.fill(result[i], true);
-		}
-		start = (N - K) / 2; // 검정색 : 시작하는 지점
-		end = (N + K) / 2 - 1; // 검정색 : 끝나는 지점
+		R1 = init(st.nextToken());
+		R2 = init(st.nextToken());
+		C1 = init(st.nextToken());
+		C2 = init(st.nextToken());
+		result = new boolean[R2 - R1 + 1][C2 - C1 + 1];
+		
+		start = (N - K) / 2;
+		end = (N + K) / 2 - 1;
 	}
 
-// main
 	public static void main(String[] args) throws IOException {
 		make();
 		solution();
+	}
+	
+	static int init(String str) {
+		return Integer.parseInt(str);
 	}
 }
