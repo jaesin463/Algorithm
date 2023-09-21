0bc87370ec57c1ad8ca1cbee23d8480efa75f343
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-09-21 23:56:12 +0900

    [Gold IV] Title: 행성 연결, Time: 928 ms, Memory: 183588 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.java
@@ -0,0 +1,107 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int V, E;
+	static long result = 0;
+	static int[][] temp;
+	static boolean[] visited;
+	static List<Vertex>[] graph;
+
+	static void solution(int start) {
+		Queue<Vertex> pq = new PriorityQueue<>();
+		pq.add(new Vertex(start, 0));
+
+		while (!pq.isEmpty()) {
+			Vertex now = pq.poll();
+
+			if (visited[now.next])
+				continue;
+			visited[now.next] = true;
+			result += now.value;
+
+			for (Vertex v : graph[now.next]) {
+				if (!visited[v.next]) {
+					pq.add(v);
+				}
+			}
+		}
+	}
+	
+	static void makeGraph() throws IOException{
+//		st = new StringTokenizer(br.readLine());
+
+//		E = init(st.nextToken());
+
+		graph = new ArrayList[V + 1];
+		visited = new boolean[V + 1];
+
+		for (int i = 1; i <= V; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for(int i = 1; i < V; i++) {
+			for(int j = i + 1; j < V + 1; j++) {
+				graph[i].add(new Vertex(j, temp[i][j]));
+				graph[j].add(new Vertex(i, temp[i][j]));
+			}
+		}
+//		
+//		for (int i = 0; i < E; i++) {
+//			st = new StringTokenizer(br.readLine());
+//			int now = init(st.nextToken());
+//			int next = init(st.nextToken());
+//			int value = init(st.nextToken());
+//
+//			graph[now].add(new Vertex(next, value));
+//			graph[next].add(new Vertex(now, value));
+//		}
+	}
+
+	static void make() throws IOException {
+		V = init(br.readLine());
+		temp = new int[V + 1][V + 1];
+		for(int i = 1; i < V + 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1; j < V + 1; j++) {
+				temp[i][j] = init(st.nextToken());
+			}
+		}
+		makeGraph();
+	}
+
+	public static void main(String[] args) throws IOException {
+		make();
+		solution(1);
+		System.out.println(result);
+	}
+
+	static int init(String str) {
+		return Integer.parseInt(str);
+	}
+
+	static class Vertex implements Comparable<Vertex> {
+		int next;
+		int value;
+
+		public Vertex(int next, int value) {
+			this.next = next;
+			this.value = value;
+		}
+
+		@Override
+		public int compareTo(Vertex v) {
+			return this.value - v.value;
+		}
+	}
+
+}
