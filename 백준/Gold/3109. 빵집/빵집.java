72ad76756c141bf13112eeacfd66a0ad86d86842
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-10-17 11:27:59 +0900

    [Gold II] Title: 빵집, Time: 320 ms, Memory: 36848 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -4,50 +4,74 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int r,c,cnt=0;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int N, M, cnt = 0;
 	static char[][] map;
 	static boolean[][] check;
-	static int[] dr= {-1,0,1}; // 출발점 0~r-1이므로, 도착점 0~r-1순대로 도착하게 탐색 방향 설정 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-	
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		map =new char[r][c];
-		
-		for(int i=0; i<r; i++) {
-			String line = br.readLine();
-			for(int j=0; j<c; j++) {
-				map[i][j] = line.charAt(j);
+	// 우측으로만 이동하기 때문에 column은 항상 1씩 증가할 거임
+	static int[] dr = { -1, 0, 1 }; // 출발점 0~r-1이므로, 도착점 0~r-1순대로 도착하게 탐색 방향 설정
+
+	static boolean dfs(int row, int col) {
+		for (int i = 0; i < 3; i++) {
+			int nr = row + dr[i];
+			int nc = col + 1;
+
+			if (!index(nr, nc))
+				continue;
+			if (map[nr][nc] == '.') {
+				if (nc == M - 1) {
+					cnt++;
+					return true;
+				}
+
+				map[nr][nc] = '-';
+				if (dfs(nr, nc))
+					return true;
 			}
+
 		}
-		
-		for(int i=0; i<r; i++) {
-			dfs(0,i);
+		return false;
+	}
+
+	static void solution() {
+		for (int i = 0; i < N; i++) {
+			dfs(i, 0);
 		}
 		System.out.println(cnt);
 	}
-	
-	static boolean dfs(int x, int y) {
-	
-		for(int i=0; i<3; i++) {
-			int nx = x + 1;
-			int ny = y + dr[i];
-			
-			if(nx<0 || nx>c-1 || ny<0 || ny>r-1) continue;
-			if(map[ny][nx] =='.') {
-				if(nx == c-1) {
-					cnt ++;
-					return true;
-				}
-				
-				map[ny][nx] = '-';
-  			   if(dfs(nx,ny)) return true;
+
+	static void make() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = init(st);
+		M = init(st);
+		map = new char[N][M];
+
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = line.charAt(j);
 			}
-			
 		}
-		return false;	
+	}
+
+	public static void main(String[] args) throws IOException {
+		make();
+		solution();
+	}
+
+	static boolean index(int r, int c) {
+		if (r < 0 || r >= N || c < 0 || c >= M) {
+			return false;
+		}
+		return true;
+	}
+
+	static int init() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+
+	static int init(StringTokenizer st) {
+		return Integer.parseInt(st.nextToken());
 	}
 }
