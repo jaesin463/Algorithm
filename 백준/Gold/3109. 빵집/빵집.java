d50abc47a251d290736a462d77956aedf3970464
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-10-17 11:41:29 +0900

    [Gold II] Title: 빵집, Time: 320 ms, Memory: 36828 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -10,22 +10,28 @@ public class Main {
 	static char[][] map;
 	static boolean[][] check;
 	// 우측으로만 이동하기 때문에 column은 항상 1씩 증가할 거임
-	static int[] dr = { -1, 0, 1 }; // 출발점 0~r-1이므로, 도착점 0~r-1순대로 도착하게 탐색 방향 설정
+	// 도착을 위에서부터 하도록 상, 중, 하
+	static int[] dr = { -1, 0, 1 };
 
 	static boolean dfs(int row, int col) {
+		// 이동 할 수 있는 경우의 수 3가지
 		for (int i = 0; i < 3; i++) {
 			int nr = row + dr[i];
 			int nc = col + 1;
-
-			if (!index(nr, nc))
-				continue;
-			if (map[nr][nc] == '.') {
+			// 인덱스 범위 내 파이프가 설치되지 않은 빈칸
+			if (index(nr, nc) && map[nr][nc] == '.') {
+				// 마지막 열에 도착
 				if (nc == M - 1) {
+					// 연결된 파이프 개수 증가
 					cnt++;
+					// 더 이상 이번 타임의 dfs가 실행되지 않도록 return 해줌
 					return true;
 				}
-
-				map[nr][nc] = '-';
+				
+				// 파이프 설치 표현
+				// 한 번 실패한 위치는 나중에 다시 방문해도 실패이므로
+				// 다시 원래대로 안바꾼느게 시간 절약
+				map[nr][nc] = '0';
 				if (dfs(nr, nc))
 					return true;
 			}
