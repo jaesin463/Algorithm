491629816d47ef5fe1c0ffb4562881014dfa8e46
XinXe <117919180+jaesin463@users.noreply.github.com>
2023-11-07 10:32:43 +0900

    [Gold I] Title: 독수리, Time: 112 ms, Memory: 11928 KB -BaekjoonHub

---
+++ 백준/Gold/16238. 독수리/독수리.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static int N, sheep[];
+	static int result = 0;
+
+	static int findMax() {
+		int idx = 1;
+		for (int i = 2; i < N + 1; i++) {
+			if (sheep[i] > sheep[idx]) {
+				idx = i;
+			}
+		}
+		return idx;
+	}
+
+	static void solution() throws IOException {
+		for (int i = 0; i < N; i++) {
+			int idx = findMax();
+			int max = sheep[idx];
+			result += Math.max(max - i, 0);
+			sheep[idx] = 0;
+		}
+		System.out.println(result);
+	}
+
+	static void make() throws IOException {
+		N = init();
+
+		sheep = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i < N + 1; i++) {
+			sheep[i] = init(st);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		make();
+		solution();
+	}
+
+	static int init() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+
+	static int init(StringTokenizer st) {
+		return Integer.parseInt(st.nextToken());
+	}
+}
