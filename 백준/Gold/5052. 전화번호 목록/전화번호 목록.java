44b4c6affe554a45523d696207beb05698975854
XinXe <117919180+jaesin463@users.noreply.github.com>
2024-01-14 02:31:14 +0900

    [Gold IV] Title: 전화번호 목록, Time: 524 ms, Memory: 240544 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static class TrieNode {
+        Map<Character, TrieNode> childNode = new HashMap<>();
+        boolean terminal;
+
+        TrieNode() {
+        }
+
+        public void insert(String word) {
+            TrieNode trieNode = this;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+
+                // tmp childNode에 c없으면 추가
+                trieNode.childNode.putIfAbsent(c, new TrieNode());
+                trieNode = trieNode.childNode.get(c);
+
+                // 마지막 문자 terminal
+                if (i == word.length() - 1) {
+                    trieNode.terminal = true;
+                    return;
+                }
+            }
+        }
+
+        public boolean contains(String word) {
+            TrieNode trieNode = this;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                TrieNode node = trieNode.childNode.get(c);
+
+                if (node == null) {
+                    return false;
+                }
+                trieNode = node;
+            }
+
+            // 해당 단어로 종료하는 문자가 있는 경우 false
+            if (trieNode.terminal) {
+                if (trieNode.childNode.isEmpty()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int t = init();
+
+        for (int i = 0; i < t; i++) {
+            int n = init();
+            TrieNode trie = new TrieNode();
+            boolean isConsistent = true;
+
+            List<String> keyList = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                String str = br.readLine();
+                trie.insert(str);
+                keyList.add(str);
+            }
+
+            for (String key : keyList) {
+                if (trie.contains(key)) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+            System.out.println(isConsistent ? "YES" : "NO");
+        }
+    }
+
+    static int init() throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+
+    static int init(StringTokenizer st) {
+        return Integer.parseInt(st.nextToken());
+    }
+}
